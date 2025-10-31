af2f034514342cabe22d9da09873583233bda38c
duytan124 <duytan2005hg@gmail.com>
2025-10-31 13:06:43 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,22 @@ const routes = [
         path: "/",
         name: "contactbook",
         component: ContactBook,
+
     },
     {
         path: "/:pathMatch(.*)*",
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+    },
 ];
-
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
-export default router; 
+export default router;
